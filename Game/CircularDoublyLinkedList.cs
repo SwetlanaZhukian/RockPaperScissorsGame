a10e9ba10fe1657ca58329aac0465c3291b86534
Your Name <you@example.com>
2020-10-07 21:40:14 +0300

    all game

---
+++ Game/CircularDoublyLinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Game
+{
+    public class CircularDoublyLinkedList<T>: IEnumerable<T>
+    {
+        Node<T> head;
+        int count;
+        public int Count { get { return count; } }
+        public Node<T> Head { get; set; }
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = node;
+                head.Next = node;
+                head.Previous = node;
+            }
+            else
+            {
+                node.Previous = head.Previous;
+                node.Next = head;
+                head.Previous.Next = node;
+                head.Previous = node;
+            }
+            count++;
+        }
+
+        public Node<T> Find(T item)
+        {
+            Node<T> node = FindNode(head, item);
+            return node;
+
+        }
+
+        Node<T> FindNode(Node<T> node, T valueToCompare)
+        {
+            Node<T> result = null;
+            if (Equals(node.Data, valueToCompare))
+            {
+                result = node;
+            }
+            else if (result == null && node.Next != head)
+            {
+                result = FindNode(node.Next, valueToCompare);
+            }
+            return result;
+        }
+    
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            do
+            {
+                if (current != null)
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                }
+            }
+            while (current != head);
+        }
+    }
+}
---
+++ Game/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length < 3 || args.Length % 2 == 0)
+            {
+                Console.WriteLine("Invalid input!");
+                return;
+
+            }
+
+            for (int i = 0; i <= args.Length; i++)
+            {
+                for (int j = i + 1; j < args.Length; j++)
+                {
+                    if (args[i] == args[j])
+                    {
+                        Console.WriteLine("Invalid input!");
+                        return;
+
+                    }
+                }
+            }
+
+            bool invalidInput = true;
+            int index = 1;
+            string userMove = "";
+            string key = GenerateRandomCryptographicKey(32);
+            string computerMove = GenerateRandomMove(args);
+  
+            Console.WriteLine("HMAC: " + GeherateHMAC(key, computerMove));
+            Console.WriteLine("Available moves:");
+
+            CircularDoublyLinkedList<string> moves = new CircularDoublyLinkedList<string>();
+            foreach (string move in args)
+            {
+                moves.Add(move);
+            }
+             
+            foreach (string s in moves)
+            {
+                Console.WriteLine(index + " - " + s);
+                index++;
+            }
+
+            Console.WriteLine(0 + " - exit");
+
+            while (invalidInput)
+            {
+                Console.WriteLine("Enter your move: ");
+                try
+                {
+                    int selection = Convert.ToInt32(Console.ReadLine());
+
+                    if (selection == 0)
+                    {
+                        Console.WriteLine("Goodbye!");
+                        return;
+                    }
+                    else if (selection > 0 && selection <= args.Length)
+                    {
+                        userMove = args[selection - 1];
+                        Console.WriteLine("Your move: " + userMove);
+                        invalidInput = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine("Computer move: " + computerMove);
+
+            var nextItem= moves.Find(computerMove).Next;
+            var previousItem = moves.Find(computerMove).Previous;
+
+            for (int i = 0; i < (moves.Count - 1) / 2; i++)
+            {
+
+                if (moves.Find(computerMove) == moves.Find(userMove))
+                {
+                    Console.WriteLine("Draw!");
+                    break;
+                }
+                if (nextItem == moves.Find(userMove))
+                {
+                    Console.WriteLine("You win!");
+                    break;
+                }
+                 if (previousItem == moves.Find(userMove))
+                {
+                    Console.WriteLine("You lose!");
+                    break;
+                }
+                nextItem = nextItem.Next;
+                previousItem = previousItem.Previous;
+            }
+
+            Console.WriteLine("HMAC key: " + key);
+
+        }
+
+        static public string GenerateRandomCryptographicKey(int keyLength)
+        {
+            RandomNumberGenerator randomNumber = RandomNumberGenerator.Create();
+            byte[] randomBytes = new byte[keyLength];
+            randomNumber.GetBytes(randomBytes);
+            return Convert.ToBase64String(randomBytes);
+        }
+
+        static public string GenerateRandomMove(string[] moves)
+        {
+            Random random = new Random();
+            int index = random.Next(moves.Length);
+            return moves[index];
+
+        }
+
+        static public string GeherateHMAC(string key, string message)
+        {
+            var decodedKey = Encoding.Default.GetBytes(key);
+
+            using (HMACSHA256 hmac = new HMACSHA256(decodedKey))
+            {
+                var messageBytes = Encoding.Default.GetBytes(message);
+                var hash = hmac.ComputeHash(messageBytes);
+                return string.Concat(Array.ConvertAll(hash, b => b.ToString("X2"))).ToLower();
+            }
+
+        }
+    }
+}
